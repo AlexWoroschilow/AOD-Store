8033b3424ce6233c0596d83b292d594cc18e14b7
AlexWoroschilow <alex.woroschilow@gmail.com>
2019-12-07 12:03:28 +0100

    =scripts for the mysql-workbench

---
+++ MysqlWorkbench/test/test.sh
@@ -0,0 +1,9 @@
+sudo apt-get download mysql-workbench libproj12
+for i in $(apt-cache depends mysql-workbench | grep -E 'Hängt ab von:' | cut -d ':' -f 2,3 | sed -e s/'<'/''/ -e s/'>'/''/); do sudo apt-get download $i 2>>errors.txt; done
+for i in $(ls -a | grep deb ); do dpkg-deb -x $i lib ; done
+rm -rf ../AppDir/lib/*
+rm -rf ../AppDir/opt/application/*
+cp -r lib/lib/x86_64-linux-gnu/* ../AppDir/lib
+cp -r lib/usr ../AppDir/
+rm -f *.deb
+rm -rf lib errors.txt
